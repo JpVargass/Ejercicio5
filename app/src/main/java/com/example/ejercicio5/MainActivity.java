1eb6c449176f15def8ba6156a41b55575b78fab2
JpVargass <136398972+JpVargass@users.noreply.github.com>
2023-06-19 21:23:49 -0400

    Aplicación funcionando :-)

--- app/src/main/java/com/example/ejercicio5/MainActivity.java
+++ app/src/main/java/com/example/ejercicio5/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.ejercicio5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +18,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initListeners() {
-    }
+        Button showToast = findViewById(R.id.buttonShowToast);
+        Button countToast = findViewById(R.id.butonShowcount);
+        TextView textView = findViewById(R.id.texto);
+
+        showToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(),"hello toast", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+      countToast.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+
+
+           Integer numero = Integer.parseInt(textView.getText().toString());
+           numero++;
+           textView.setText(numero.toString());
+        }
+    });
+}
 }
